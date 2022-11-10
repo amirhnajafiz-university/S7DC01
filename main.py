c3fb218c1e5e60d7f40ae364ad82112a3d256ca3
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-10 21:39:19 +0330

    add: signals for mod

--- main.py
+++ main.py
@@ -9,16 +9,24 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     t = np.linspace(-0.5, 0.5, 500, endpoint=False)
-    sig = signal.square(2 * np.pi * 5 * t)
+    sig = 0.5 * (signal.square(2 * np.pi * 5 * t) + 1)
 
-    plt.subplot(2, 1, 1)
+    plt.subplot(3, 1, 1)
 
+    plt.title("input pulse")
     plt.plot(t, sig)
     plt.ylim(-2, 2)
 
-    plt.subplot(2, 1, 2)
+    plt.subplot(3, 1, 2)
 
+    plt.title("carrier")
     plt.plot(t, np.cos(2 * np.pi * t))
+    plt.ylim(-2, 2)
+
+    plt.subplot(3, 1, 3)
+
+    plt.title("output signal")
+    plt.plot(t, np.multiply(sig, np.cos(2 * np.pi * t)))
     plt.ylim(-2, 2)
 
     plt.show()
