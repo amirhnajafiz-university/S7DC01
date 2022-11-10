cc518e1e0222ab383378bcfeb11c60bfb61f590d
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-10 21:33:54 +0330

    add: cos signal

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ if __name__ == "__main__":
     t = np.linspace(-0.5, 0.5, 500, endpoint=False)
     sig = signal.square(2 * np.pi * 5 * t)
 
+    plt.subplot(2, 1, 1)
+
     plt.plot(t, sig)
     plt.ylim(-2, 2)
+
+    plt.subplot(2, 1, 2)
+
+    plt.plot(t, np.cos(2 * np.pi * t))
+    plt.ylim(-2, 2)
+
     plt.show()
