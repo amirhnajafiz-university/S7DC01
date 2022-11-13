d3b36d81963c823b1c4cf7eec35101382ea42a72
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-13 21:50:41 +0330

    feat: draw method of plotter

--- plot.py
+++ plot.py
@@ -1,2 +1,13 @@
+# importing matplotlib for displaying signals
+import matplotlib.pyplot as plt
+
+
+
 class Plotter:
-    pass
+    def draw(x, y, title, pos, yplus, ynegv):
+        plt.subplot(3, 1, pos)
+
+        plt.title(title)
+        plt.plot(x, y)
+        
+        plt.ylim(ynegv, yplus)
